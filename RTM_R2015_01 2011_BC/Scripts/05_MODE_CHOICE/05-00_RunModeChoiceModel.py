2422bfe18169c7c97da361dbab7c32319755a13c
Harvey Harrison <hharrison@mcelhanney.com>
2016-02-15 18:03:16 -0800

    RTM: convert HomeBasePersonalBusiness to self methods
    Signed-off-by: Harvey Harrison <hharrison@mcelhanney.com>

--- RTM_R2015_01 2011_BC/Scripts/05_MODE_CHOICE/05-00_RunModeChoiceModel.py
+++ RTM_R2015_01 2011_BC/Scripts/05_MODE_CHOICE/05-00_RunModeChoiceModel.py
@@ -62,7 +62,7 @@ class ModeChoice(_m.Tool()):
         home_base_work = _m.Modeller().tool("translink.emme.stage3.step5.modechoicehbw")
         home_base_school = _m.Modeller().tool("translink.emme.stage3.step5.modechoicehbschool")
         home_base_shopping = _m.Modeller().tool("translink.emme.stage3.step5.modechoicehbshop")
-        home_base_personal_business = module_lookup("translink.emme.stage3.step5.modechoicehbpersonalbusiness")
+        home_base_personal_business = _m.Modeller().tool("translink.emme.stage3.step5.modechoicehbpersonalbusiness")
         home_base_university = module_lookup("translink.emme.stage3.step5.modechoicehbuniversity")
         home_base_social = module_lookup("translink.emme.stage3.step5.modechoicehbsocial")
         home_base_escort = module_lookup("translink.emme.stage3.step5.modechoicehbesc")
