386458daf48cd1a53bf40209ed56064793fc455c
Harvey Harrison <hharrison@mcelhanney.com>
2016-08-23 16:26:22 -0700

    RTM: modify truck model to only calculate demand and PCE
    Remove the call to the detailed truck assignment module for now.
    
    Signed-off-by: Harvey Harrison <hharrison@mcelhanney.com>

--- RTM_R2015_01 2011_BC/Scripts/10_TruckModelScripts/10_01_Full Truck Model Run.py
+++ RTM_R2015_01 2011_BC/Scripts/10_TruckModelScripts/10_01_Full Truck Model Run.py
@@ -51,5 +51,46 @@ class FullTruckModel(_m.Tool()):
         RegionalModel=_m.Modeller().tool("translink.emme.stage5.step10.regionaltruck")
         RegionalModel(eb)
 
-        TruckAssign=_m.Modeller().tool("translink.emme.stage5.step10.truckassign")
-        TruckAssign(eb, AMScenario, MDScenario)
+        self.aggregate_demand_pce()
+
+    def aggregate_demand_pce(self):
+        util = _m.Modeller().tool("translink.emme.util")
+
+        util.initmat(eb, "mf1040", "CBLgAp", "CB LgTruck AM PCE", 0)
+        util.initmat(eb, "mf1041", "CBHvAp", "CB HvTruck AM PCE", 0)
+        util.initmat(eb, "mf1042", "IRLgAp", "IR LgTruck AM PCE", 0)
+        util.initmat(eb, "mf1043", "IRHvAp", "IR HvTruck AM PCE", 0)
+        util.initmat(eb, "mf1044", "APHvAp", "AP HvTruck AM PCE", 0)
+        util.initmat(eb, "mf1045", "RGLgAp", "RG LgTruck AM PCE", 0)
+        util.initmat(eb, "mf1046", "RGHvAp", "RG HvTruck AM PCE", 0)
+        util.initmat(eb, "mf1047", "CBLgMp", "CB LgTruck MD PCE", 0)
+        util.initmat(eb, "mf1048", "CBHvMp", "CB HvTruck MD PCE", 0)
+        util.initmat(eb, "mf1049", "IRLgMp", "IR LgTruck MD PCE", 0)
+        util.initmat(eb, "mf1050", "IRHvMp", "IR HvTruck MD PCE", 0)
+        util.initmat(eb, "mf1051", "APHvMp", "AP HvTruck MD PCE", 0)
+        util.initmat(eb, "mf1052", "RGLgMp", "RG LgTruck MD PCE", 0)
+        util.initmat(eb, "mf1053", "RGHvMp", "RG HvTruck MD PCE", 0)
+
+        AMList1=["mf1002","mf1012","mf1035","mf1005","mf1013","mf1021","mf1037"]
+
+        AMList2=["mf1040","mf1042","mf1045","mf1041","mf1043","mf1044","mf1046"]
+
+        MDList1=["mf1003","mf1014","mf1036","mf1006","mf1015","mf1022","mf1038"]
+
+        MDList2=["mf1047","mf1049","mf1052","mf1048","mf1050","mf1051","mf1053"]
+
+        Ratios=[1.5,1.5,1.5,2.5,2.5,2.5,2.5]
+
+        specs = []
+        for i in range (len(AMList1)):
+            specs.append(util.matrix_spec(AMList2[i], AMList1[i] + "*" + str(Ratios[i])))
+            specs.append(util.matrix_spec(MDList2[i], MDList1[i] + "*" + str(Ratios[i])))
+
+        util.compute_matrix(specs)
+
+        specs = []
+        specs.append(util.matrix_spec("mf980", "mf1002 + mf1012 + mf1035"))
+        specs.append(util.matrix_spec("mf981", "mf1005 + mf1013 + mf1021 + mf1037"))
+        specs.append(util.matrix_spec("mf982", "mf1003 + mf1014 + mf1036"))
+        specs.append(util.matrix_spec("mf983", "mf1006 + mf1015 + mf1022 + mf1038"))
+        util.compute_matrix(specs)
