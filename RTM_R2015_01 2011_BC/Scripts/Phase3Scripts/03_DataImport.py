8027e0b317e3a9d51c8f724b88a9a4593a5aa9a9
Reid Keller <reid.keller@translink.ca>
2016-10-28 16:26:01 -0700

    P3: update dataimport page method to allow file selection Tool can now be run individually or from call

--- RTM_R2015_01 2011_BC/Scripts/Phase3Scripts/03_DataImport.py
+++ RTM_R2015_01 2011_BC/Scripts/Phase3Scripts/03_DataImport.py
@@ -19,7 +19,12 @@ import pandas as pd
 
 
 class DataImport(_m.Tool()):
+
+    demographics_file = _m.Attribute(_m.InstanceType)
+    geographics_file = _m.Attribute(_m.InstanceType)
+
     tool_run_msg = _m.Attribute(unicode)
+
 
     def page(self):
         pb = _m.ToolPageBuilder(self)
@@ -27,17 +32,34 @@ class DataImport(_m.Tool()):
         pb.description = "Imports Scalar and Vector Data for Model Run"
         pb.branding_text = "TransLink"
 
+        util = _m.Modeller().tool("translink.emme.util")
+        input_path = util.get_input_path(_m.Modeller().emmebank)
+
+        pb.add_select_file(tool_attribute_name="demographics_file",
+                           window_type="file",
+                           file_filter="*.csv",
+                           start_path= input_path,
+                           title="Demographics File: ",
+                           note="File must be csv file.")
+
+        pb.add_select_file(tool_attribute_name="geographics_file",
+                           window_type="file",
+                           file_filter="*.csv",
+                           start_path= input_path,
+                           title="Geographics File: ",
+                           note="File must be csv file.")
+
         if self.tool_run_msg:
             pb.add_html(self.tool_run_msg)
 
         return pb.render()
 
     def run(self):
-        with _m.logbook_trace("UNDER DEV - PR Impedance"):
+        with _m.logbook_trace("Data Import"):
             self.tool_run_msg = ""
             try:
             	eb = _m.Modeller().emmebank
-            	self.__call__(eb)
+            	self.__call__(eb, self.demographics_file, self.geographics_file)
             	self.tool_run_msg = _m.PageBuilder.format_info("Tool complete")
             except Exception, e:
             	self.tool_run_msg = _m.PageBuilder.format_exception(e, _traceback.format_exc(e))
@@ -51,7 +73,76 @@ class DataImport(_m.Tool()):
 
     @_m.logbook_trace("Initializing Scalar Matrices")
     def init_scalars(self, eb):
-		util = _m.Modeller().tool("translink.emme.util")
+        util = _m.Modeller().tool("translink.emme.util")
+
+        util.initmat(eb, "ms100", "autoOpCost", "Auto Operating Cost", 0.18)
+        util.initmat(eb, "ms101", "lgvOpCost", "Light Truck Operating Cost", 0.24)
+        util.initmat(eb, "ms102", "hgvOpCost", "Heavy Truck Operating Cost", 0.56)
+        util.initmat(eb, "ms110", "HOV3OccHbw", "HOV3 Occupancy HB Work", 3.51)
+        util.initmat(eb, "ms111", "HOV3OccHbu", "HOV3 Occupancy HB University", 3.43)
+        util.initmat(eb, "ms112", "HOV3OccHbesc", "HOV3 Occupancy HB Escorting", 3.51)
+        util.initmat(eb, "ms113", "HOV3OccHbpb", "HOV3 Occupancy HB Pers Bus", 3.45)
+        util.initmat(eb, "ms114", "HOV3OccHBsch", "HOV3 Occupancy HB School", 3.58)
+        util.initmat(eb, "ms115", "HOV3OccHBshop", "HOV3 Occupancy HB Shop", 3.43)
+        util.initmat(eb, "ms116", "HOV3OccHBsoc", "HOV3 Occupancy HB Social", 3.68)
+        util.initmat(eb, "ms117", "HOV3OccNHBw", "HOV3 Occupancy NHB Work", 3.47)
+        util.initmat(eb, "ms118", "HOV3OccNHBo", "HOV3 Occupancy NHB Other", 3.63)
+        util.initmat(eb, "ms130", "lgvPCE", "Light Truck Passenger Car Equivalent", 1.5)
+        util.initmat(eb, "ms131", "hgvPCE", "Heavy Truck Passenger Car Equivalent", 2.5)
+        util.initmat(eb, "ms200", "VotWkLowIncSov", "VOTSOV Work Low", 3.29)
+        util.initmat(eb, "ms201", "VotWkMedIncSov", "VOTSOV Work Med", 2.06)
+        util.initmat(eb, "ms202", "VotWkHighIncSov", "VOTSOV Work High", 1.69)
+        util.initmat(eb, "ms203", "VotWkLowIncHov", "VOTHOV Work Low", 3.29)
+        util.initmat(eb, "ms204", "VotWkMedIncHov", "VOTHOV Work Med", 2.06)
+        util.initmat(eb, "ms205", "VotWkHighIncHov", "VOTHOV Work High", 1.69)
+        util.initmat(eb, "ms206", "VotNwkLowIncSov", "VOTSOV Nonwork Low", 0)
+        util.initmat(eb, "ms207", "VotNwkMedIncSov", "VOTSOV Nonwork Med", 0)
+        util.initmat(eb, "ms208", "VotNwkHighIncSov", "VOTSOV Nonwork High", 0)
+        util.initmat(eb, "ms209", "VotNwkLowIncHov", "VOTHOV Nonwork Low", 0)
+        util.initmat(eb, "ms210", "VotNwkMedIncHov", "VOTHOV Nonwork Med", 0)
+        util.initmat(eb, "ms211", "VotNwkHighIncHov", "VOTHOV Nonwork High", 0)
+        util.initmat(eb, "ms212", "VotWkBus", "VOT Bus Work", 3.06)
+        util.initmat(eb, "ms213", "VotWkRail", "VOT Rail Work", 3.35)
+        util.initmat(eb, "ms214", "VotWkWce", "VOT WCE Work", 3.35)
+        util.initmat(eb, "ms215", "VotNwkBus", "VOT Bus Nonwork", 0)
+        util.initmat(eb, "ms216", "VotNwkRail", "VOT Rail Nonwork", 0)
+        util.initmat(eb, "ms217", "VotNwkWce", "VOT WCE Nonwork", 0)
+        util.initmat(eb, "ms218", "VotLgv", "VOT lgv", 2.03)
+        util.initmat(eb, "ms219", "VotHgv", "VOT hgv", 1.43)
+        util.initmat(eb, "ms300", "busIVTprcpWk", "bus in-vehicle time perception work", 1.0922)
+        util.initmat(eb, "ms301", "busWAITprcpWk", "bus wait time perception work", 1.6543)
+        util.initmat(eb, "ms302", "busWALKprcpWk", "bus walk time perception work", 1.3765)
+        util.initmat(eb, "ms303", "busTRANSprcpWk", "bus transfer perception work", 10.939)
+        util.initmat(eb, "ms304", "busBOARDSprcpWk", "bus boarding perception work", 0)
+        util.initmat(eb, "ms310", "railIVTprcpWk", "rail in-vehicle time perception work", 1)
+        util.initmat(eb, "ms311", "railWAITprcpWk", "rail wait time perception work", 1.8068)
+        util.initmat(eb, "ms312", "railWALKprcpWk", "rail walk time perception work", 1.5035)
+        util.initmat(eb, "ms313", "railTRANSprcpWk", "rail transfer perception work", 11.948)
+        util.initmat(eb, "ms314", "railBOARDSprcpWk", "rail boarding perception work", 0)
+        util.initmat(eb, "ms320", "wceIVTprcpWk", "wce in-vehicle time perception work", 1)
+        util.initmat(eb, "ms321", "wceWAITprcpWk", "wce wait time perception work", 1.8068)
+        util.initmat(eb, "ms322", "wceWALKprcpWk", "wce walk time perception work", 1.5035)
+        util.initmat(eb, "ms323", "wceTRANSprcpWk", "wce transfer perception work", 11.948)
+        util.initmat(eb, "ms324", "wceBOARDSprcpWk", "wce boarding perception work", 0)
+        util.initmat(eb, "ms330", "busIVTprcpNwk", "bus in-vehicle time perception nonwork", 0)
+        util.initmat(eb, "ms331", "busWAITprcpNwk", "bus wait time perception nonwork", 0)
+        util.initmat(eb, "ms332", "busWALKprcpNwk", "bus walk time perception nonwork", 0)
+        util.initmat(eb, "ms333", "busTRANSprcpNwk", "bus transfer perception nonwork", 0)
+        util.initmat(eb, "ms334", "busBOARDSprcpNwk", "bus boarding perception nonwork", 0)
+        util.initmat(eb, "ms340", "railIVTprcpNwk", "rail in-vehicle time perception nonwork", 0)
+        util.initmat(eb, "ms341", "railWAITprcpNwk", "rail wait time perception nonwork", 0)
+        util.initmat(eb, "ms342", "railWALKprcpNwk", "rail walk time perception nonwork", 0)
+        util.initmat(eb, "ms343", "railTRANSprcpNwk", "rail transfer perception nonwork", 0)
+        util.initmat(eb, "ms344", "railBOARDSprcpNwk", "rail boarding perception nonwork", 0)
+        util.initmat(eb, "ms350", "wceIVTprcpNwk", "wce in-vehicle time perception nonwork", 0)
+        util.initmat(eb, "ms351", "wceWAITprcpNwk", "wce wait time perception nonwork", 0)
+        util.initmat(eb, "ms352", "wceWALKprcpNwk", "wce walk time perception nonwork", 0)
+        util.initmat(eb, "ms353", "wceTRANSprcpNwk", "wce transfer perception nonwork", 0)
+        util.initmat(eb, "ms354", "wceBOARDSprcpNwk", "wce boarding perception nonwork", 0)
+
+
+
+
 
 
     @_m.logbook_trace("Importing Vector Data from CSV")
