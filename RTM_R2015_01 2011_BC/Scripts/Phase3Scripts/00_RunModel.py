4318282645c0fd7b5233537bb1b65d709fc17a87
Harvey Harrison <hharrison@mcelhanney.com>
2016-11-09 16:49:12 -0800

    RTM: fix small spelling error in Convergence Criteria matrix name
    Signed-off-by: Harvey Harrison <hharrison@mcelhanney.com>

--- RTM_R2015_01 2011_BC/Scripts/Phase3Scripts/00_RunModel.py
+++ RTM_R2015_01 2011_BC/Scripts/Phase3Scripts/00_RunModel.py
@@ -148,5 +148,5 @@ class FullModelRun(_m.Tool()):
         util.initmat(eb, "ms40", "IterAss", "Assignment Iterations", max_assignment_iterations)
         util.initmat(eb, "ms41", "ConRelGap", "ConvergenceRelativeGap", 0.0001)
         util.initmat(eb, "ms42", "ConBestRel", "ConvergenceBestRelative", 0.01)
-        util.initmat(eb, "ms43", "ConNorm", "ConvergaenceNormalized", 0.005)
+        util.initmat(eb, "ms43", "ConNorm", "ConvergenceNormalized", 0.005)
         util.initmat(eb, "ms44", "AutoOcc", "Standard HOV Occupancy", 2.4)
