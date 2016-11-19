72a6872090b0cc2d4b63c555a6dd7c2f90c4aff7
Ali Darwiche <ali.darwiche@translink.ca>
2016-11-18 17:13:52 -0800

    Phase 3 - AD: deleted empty space

--- RTM_R2015_01 2011_BC/Scripts/Phase3Scripts/02_DataImport.py
+++ RTM_R2015_01 2011_BC/Scripts/Phase3Scripts/02_DataImport.py
@@ -93,7 +93,6 @@ class DataImport(_m.Tool()):
         util.initmat(eb, "ms118", "HOV3OccNHBo", "HOV3 Occupancy NHB Other", 3.63)
         util.initmat(eb, "ms130", "lgvPCE", "Light Truck Passenger Car Equivalent", 1.5)
         util.initmat(eb, "ms131", "hgvPCE", "Heavy Truck Passenger Car Equivalent", 2.5)
-
         util.initmat(eb, "ms200", "VotWkLowIncSov", "VOTSOV Work Low"      , 6.58)
         util.initmat(eb, "ms201", "VotWkMedIncSov", "VOTSOV Work Med"      , 4.12)
         util.initmat(eb, "ms202", "VotWkHighIncSov", "VOTSOV Work High"    , 3.38)
