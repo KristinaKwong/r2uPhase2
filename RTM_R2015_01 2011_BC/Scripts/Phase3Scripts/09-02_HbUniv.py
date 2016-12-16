Phase 3:AD- Fixed generalized cost formula for transit and densities

RTM_R2015_01 2011_BC/Scripts/Phase3Scripts/09-02_HbUniv.py
@@ -134,6 +134,7 @@ class HbWork(_m.Tool()):
         # Generate Dataframe
         Df = {}
         Tiny=0.000001
+        UPass_Disc = 0.1
         ##
         ##    Bus and rail related variables for University purpose
         ##
@@ -161,7 +162,7 @@ class HbWork(_m.Tool()):
         # Bus Utility
         # Bus Utility for all incomes
         Df['GeUtl'] = ( p4
-                      + p12*(Df['BusFar'])*0.1
+                      + p12*(Df['BusFar'])*UPass_Disc
                       + p15*Df['BusIVT']
                       + p17*Df['BusWat']
                       + p18*Df['BusAux']
@@ -174,7 +175,7 @@ class HbWork(_m.Tool()):
         ##
         Df['GeUtl'] = ( p4*Df['RalIBR']
                       + p6*Df['RalIRR']
-                      + p12*(Df['RalFar'])*0.1
+                      + p12*(Df['RalFar'])*UPass_Disc
                       + p15*Df['RalIVB']
                       + p15*Df['RalIVR']
                       + p17*Df['RalWat']
@@ -216,7 +217,7 @@ class HbWork(_m.Tool()):
 #        ##############################################################################
         ## Add SOV Availability Term
 
-        CarShare = util.get_matrix_numpy(eb, 'cs500').reshape(NoTAZ,1) + np.zeros((1, NoTAZ))
+
         LrgU     = -99999.0
         ## Low Income Zero Autos
         Dict = {
@@ -501,3 +502,68 @@ class HbWork(_m.Tool()):
 
         ## Initialize P-A Trip Tables from trip distribution
         util.initmat(eb, "mf3150", "HbUP-A", " HbU P-A Trips ", 0)
+
+       #Temporary Location for peak hour matrices
+        util.initmat(eb, "mf200", "SOV_pertrp_VOT_1_Am", "SOV per-trips VOT 1 AM", 0)
+        util.initmat(eb, "mf201", "SOV_pertrp_VOT_2_Am", "SOV per-trips VOT 2 AM", 0)
+        util.initmat(eb, "mf202", "SOV_pertrp_VOT_3_Am", "SOV per-trips VOT 3 AM", 0)
+        util.initmat(eb, "mf203", "SOV_pertrp_VOT_4_Am", "SOV per-trips VOT 4 AM", 0)
+        util.initmat(eb, "mf206", "HOV_pertrp_VOT_1_Am", "HOV per-trips VOT 1 AM", 0)
+        util.initmat(eb, "mf207", "HOV_pertrp_VOT_2_Am", "HOV per-trips VOT 2 AM", 0)
+        util.initmat(eb, "mf208", "HOV_pertrp_VOT_3_Am", "HOV per-trips VOT 3 AM", 0)
+        util.initmat(eb, "mf209", "HOV_pertrp_VOT_4_Am", "HOV per-trips VOT 4 AM", 0)
+        util.initmat(eb, "mf212", "Bus_pertrp_Am", "Bus per-trips AM", 0)
+        util.initmat(eb, "mf213", "Rail_pertrp_Am", "Rail per-trips AM", 0)
+        util.initmat(eb, "mf214", "WCE_pertrp_Am", "WCE per-trips AM", 0)
+        util.initmat(eb, "mf215", "Wk_pertrp_Am", "Walk per-trips AM", 0)
+        util.initmat(eb, "mf216", "Bk_pertrp_Am", "Bike per-trips AM", 0)
+        util.initmat(eb, "mf230", "SOV_pertrp_VOT_1_Md", "SOV per-trips VOT 1 MD", 0)
+        util.initmat(eb, "mf231", "SOV_pertrp_VOT_2_Md", "SOV per-trips VOT 2 MD", 0)
+        util.initmat(eb, "mf232", "SOV_pertrp_VOT_3_Md", "SOV per-trips VOT 3 MD", 0)
+        util.initmat(eb, "mf233", "SOV_pertrp_VOT_4_Md", "SOV per-trips VOT 4 MD", 0)
+        util.initmat(eb, "mf236", "HOV_pertrp_VOT_1_Md", "HOV per-trips VOT 1 MD", 0)
+        util.initmat(eb, "mf237", "HOV_pertrp_VOT_2_Md", "HOV per-trips VOT 2 MD", 0)
+        util.initmat(eb, "mf238", "HOV_pertrp_VOT_3_Md", "HOV per-trips VOT 3 MD", 0)
+        util.initmat(eb, "mf239", "HOV_pertrp_VOT_4_Md", "HOV per-trips VOT 4 MD", 0)
+        util.initmat(eb, "mf242", "Bus_pertrp_Md", "Bus per-trips MD", 0)
+        util.initmat(eb, "mf243", "Rail_pertrp_Md", "Rail per-trips MD", 0)
+        util.initmat(eb, "mf244", "WCE_pertrp_Md", "WCE per-trips MD", 0)
+        util.initmat(eb, "mf245", "Wk_pertrp_Md", "Walk per-trips MD", 0)
+        util.initmat(eb, "mf246", "Bk_pertrp_Md", "Bike per-trips MD", 0)
+        util.initmat(eb, "mf260", "SOV_pertrp_VOT_1_Pm", "SOV per-trips VOT 1 PM", 0)
+        util.initmat(eb, "mf261", "SOV_pertrp_VOT_2_Pm", "SOV per-trips VOT 2 PM", 0)
+        util.initmat(eb, "mf262", "SOV_pertrp_VOT_3_Pm", "SOV per-trips VOT 3 PM", 0)
+        util.initmat(eb, "mf263", "SOV_pertrp_VOT_4_Pm", "SOV per-trips VOT 4 PM", 0)
+        util.initmat(eb, "mf266", "HOV_pertrp_VOT_1_Pm", "HOV per-trips VOT 1 PM", 0)
+        util.initmat(eb, "mf267", "HOV_pertrp_VOT_2_Pm", "HOV per-trips VOT 2 PM", 0)
+        util.initmat(eb, "mf268", "HOV_pertrp_VOT_3_Pm", "HOV per-trips VOT 3 PM", 0)
+        util.initmat(eb, "mf269", "HOV_pertrp_VOT_4_Pm", "HOV per-trips VOT 4 PM", 0)
+        util.initmat(eb, "mf272", "Bus_pertrp_Pm", "Bus per-trips PM", 0)
+        util.initmat(eb, "mf273", "Rail_pertrp_Pm", "Rail per-trips PM", 0)
+        util.initmat(eb, "mf274", "WCE_pertrp_Pm", "WCE per-trips PM", 0)
+        util.initmat(eb, "mf275", "Wk_pertrp_Pm", "Walk per-trips PM", 0)
+        util.initmat(eb, "mf276", "Bk_pertrp_Pm", "Bike per-trips PM", 0)
+        util.initmat(eb, "mf300", "SOV_drvtrp_VOT_1_Am", "SOV drv-trips VOT 1 AM", 0)
+        util.initmat(eb, "mf301", "SOV_drvtrp_VOT_2_Am", "SOV drv-trips VOT 2 AM", 0)
+        util.initmat(eb, "mf302", "SOV_drvtrp_VOT_3_Am", "SOV drv-trips VOT 3 AM", 0)
+        util.initmat(eb, "mf303", "SOV_drvtrp_VOT_4_Am", "SOV drv-trips VOT 4 AM", 0)
+        util.initmat(eb, "mf306", "HOV_drvtrp_VOT_1_Am", "HOV drv-trips VOT 1 AM", 0)
+        util.initmat(eb, "mf307", "HOV_drvtrp_VOT_2_Am", "HOV drv-trips VOT 2 AM", 0)
+        util.initmat(eb, "mf308", "HOV_drvtrp_VOT_3_Am", "HOV drv-trips VOT 3 AM", 0)
+        util.initmat(eb, "mf309", "HOV_drvtrp_VOT_4_Am", "HOV drv-trips VOT 4 AM", 0)
+        util.initmat(eb, "mf320", "SOV_drvtrp_VOT_1_Md", "SOV drv-trips VOT 1 MD", 0)
+        util.initmat(eb, "mf321", "SOV_drvtrp_VOT_2_Md", "SOV drv-trips VOT 2 MD", 0)
+        util.initmat(eb, "mf322", "SOV_drvtrp_VOT_3_Md", "SOV drv-trips VOT 3 MD", 0)
+        util.initmat(eb, "mf323", "SOV_drvtrp_VOT_4_Md", "SOV drv-trips VOT 4 MD", 0)
+        util.initmat(eb, "mf326", "HOV_drvtrp_VOT_1_Md", "HOV drv-trips VOT 1 MD", 0)
+        util.initmat(eb, "mf327", "HOV_drvtrp_VOT_2_Md", "HOV drv-trips VOT 2 MD", 0)
+        util.initmat(eb, "mf328", "HOV_drvtrp_VOT_3_Md", "HOV drv-trips VOT 3 MD", 0)
+        util.initmat(eb, "mf329", "HOV_drvtrp_VOT_4_Md", "HOV drv-trips VOT 4 MD", 0)
+        util.initmat(eb, "mf340", "SOV_drvtrp_VOT_1_Pm", "SOV drv-trips VOT 1 PM", 0)
+        util.initmat(eb, "mf341", "SOV_drvtrp_VOT_2_Pm", "SOV drv-trips VOT 2 PM", 0)
+        util.initmat(eb, "mf342", "SOV_drvtrp_VOT_3_Pm", "SOV drv-trips VOT 3 PM", 0)
+        util.initmat(eb, "mf343", "SOV_drvtrp_VOT_4_Pm", "SOV drv-trips VOT 4 PM", 0)
+        util.initmat(eb, "mf346", "HOV_drvtrp_VOT_1_Pm", "HOV drv-trips VOT 1 PM", 0)
+        util.initmat(eb, "mf347", "HOV_drvtrp_VOT_2_Pm", "HOV drv-trips VOT 2 PM", 0)
+        util.initmat(eb, "mf348", "HOV_drvtrp_VOT_3_Pm", "HOV drv-trips VOT 3 PM", 0)
+        util.initmat(eb, "mf349", "HOV_drvtrp_VOT_4_Pm", "HOV drv-trips VOT 4 PM", 0)