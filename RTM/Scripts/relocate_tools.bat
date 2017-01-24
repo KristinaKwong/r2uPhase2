1f28289190cf6058a88946e5af9d70721d2342c7
Reid Keller <reid.keller@translink.ca>
2017-01-24 15:55:02 -0800

    P3: update relocate_tools batch files to remove phase 2 toolbox reference add phase 3 toolbox reference to relocate_tools shell script

--- RTM/Scripts/relocate_tools.bat
+++ RTM/Scripts/relocate_tools.bat
@@ -3,6 +3,5 @@ rem location of the EMME project files.
 rem
 rem Find the installed EMME python as it may not exist in $PATH
 set PYTHON=%EMMEPATH%\Python27\python.exe
-"%PYTHON%" toolbox_modify.py ../Scripts/TL_RTM_Release_2015_01.00.mtbx Scripts
 "%PYTHON%" toolbox_modify.py ../Scripts/Phase3Scripts/RTM_Phase3.mtbx Phase3Scripts
 "%PYTHON%" toolbox_modify.py ../Scripts/util/RTM_util.mtbx util
