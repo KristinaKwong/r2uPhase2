57e54b593a963262db3b6b0508dc8f376d21bbdd
Reid Keller <reid.keller@translink.ca>
2017-02-14 15:10:48 -0800

    Bring back multi threading in transit assignment.  This causes model to crash

--- RTM/Scripts/Phase3Scripts/10-02_TransitAssignment.py
+++ RTM/Scripts/Phase3Scripts/10-02_TransitAssignment.py
@@ -248,7 +248,7 @@ class TransitAssignment(_m.Tool()):
             },
             "journey_levels": [],
             "performance_settings": {
-                "number_of_processors": 1
+                "number_of_processors": self.num_processors
             },
             "type": "EXTENDED_TRANSIT_ASSIGNMENT"
         }
