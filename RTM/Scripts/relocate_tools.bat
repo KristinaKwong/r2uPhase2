4e258a568e82e4c1803dc77670212fb134977cb3
Harvey Harrison <hharrison@mcelhanney.com>
2017-03-16 10:46:37 -0700

    RTM: add a PAUSE command to the relocate scripts batch file
    This allows the user to see whether or not the relocation was successful.
    
    Signed-off-by: Harvey Harrison <hharrison@mcelhanney.com>

--- RTM/Scripts/relocate_tools.bat
+++ RTM/Scripts/relocate_tools.bat
@@ -5,3 +5,4 @@ rem Find the installed EMME python as it may not exist in $PATH
 set PYTHON=%EMMEPATH%\Python27\python.exe
 "%PYTHON%" toolbox_modify.py ../Scripts/Phase3Scripts/RTM_Phase3.mtbx Phase3Scripts
 "%PYTHON%" toolbox_modify.py ../Scripts/util/RTM_util.mtbx util
+PAUSE
