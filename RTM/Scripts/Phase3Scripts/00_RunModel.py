bcba18ae9c296256e940bf09bc02c4ccac60271e
Harvey Harrison <hharrison@mcelhanney.com>
2017-12-08 19:36:44 -0800

    RTM: fix typo in RunModel
    Signed-off-by: Harvey Harrison <hharrison@mcelhanney.com>

--- RTM/Scripts/Phase3Scripts/00_RunModel.py
+++ RTM/Scripts/Phase3Scripts/00_RunModel.py
@@ -50,7 +50,7 @@ class FullModelRun(_m.Tool()):
 
         pb.add_text_box(tool_attribute_name="horizon_year",
                         size="4",
-                        title="Model horizion year:",
+                        title="Model horizon year:",
                         note="Should match current landuse years: 2011, 2030 and 2045")
 
         pb.add_select_scenario(tool_attribute_name="master_scen",
