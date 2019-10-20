f08f52772e773ca615b671cdaad1a3811d56ac42
Siyu Lin <slin@mcelhanney.com>
2019-10-20 16:00:09 -0700

    Package tools to Analytics Toolbox
    1. [07fe1ff6eb570362fba] Auto Select Link Tool
    2. [dd9f3b33448582470ed] Tag Polyline To Link Attribute Tool
    3. [f37b2e2d1187b75c55e] Extract Variables of Interest Tool

---
+++ RTM/Scripts/Phase3Analytics/TagPolygonToLink.py
@@ -0,0 +1,133 @@
+##---------------------------------------------------------------------
+##--General EMME Python Tool
+##--Namespace: translink.internal_tool.tagpolygon
+##--Purpose: Tag polygon attribute to EMME links
+##---------------------------------------------------------------------
+import inro.modeller as _m
+import traceback as _traceback
+import fiona
+import shapely.geometry as geom
+import os.path as path
+
+class TagPolygonToLink(_m.Tool()):
+    scenario = _m.Attribute(_m.InstanceType)
+    linkattribute = _m.Attribute(_m.InstanceType)
+    polygonfile = _m.Attribute(_m.InstanceType)
+    polygonfield = _m.Attribute(_m.InstanceType)
+    algorithm = _m.Attribute(_m.InstanceType)
+    excludeconnector = _m.Attribute(bool)
+    default = _m.Attribute(float)
+
+    tool_run_msg = _m.Attribute(unicode)
+
+    def __init__(self):
+        self.default = 0
+        self.excludeconnector = True
+        self.polygonfile = path.abspath(path.join(__file__ , "..", "..", "..", "Media", "Shapefiles for RTM", "RTM_8ZonePolygon.shp"))
+        self.polygonfield = "Id"
+        self.algorithm = "by_length"
+
+    def page(self):
+        pb = _m.ToolPageBuilder(self)
+        pb.title = "Tag Polygon Attribute to EMME Link"
+        pb.description = "Selected attibute in polygon shapefile will be tagged to EMME Link"
+
+        if self.tool_run_msg:
+            pb.add_html(self.tool_run_msg)
+
+        pb.add_select_scenario("scenario", title = "Select Scenario for EMME Link Tags: ")
+        #pb.add_select_extra_attribute("linkattribute", title = "Select Link Attribute for EMME Link Tags: ")
+        pb.add_text_box(tool_attribute_name = "linkattribute", size = 20, title = "Select Link Attribute for EMME Link Tags: ")
+        pb.add_select_file(tool_attribute_name = "polygonfile", window_type = "file",file_filter="*.shp",title="Polygon Shapefile: ")
+        pb.add_text_box(tool_attribute_name = "polygonfield", size = 10, title = "Enter the field name in polygon shapefile: ")
+        pb.add_text_box(tool_attribute_name = "algorithm", size = 50, title = "Enter algorithm if there are multiple match: ",
+                        note = "tag_max; tag_min; by_length; do_not_change; set_to_-1.0")
+        pb.add_checkbox("excludeconnector", label="Exclude Centroid Connectors")
+        pb.add_text_box(tool_attribute_name = "default",title = "Default value if no match: ")
+
+        return pb.render()
+
+    def run(self):
+        self.tool_run_msg = ""
+        try:
+            #linkattributeName = self.linkattribute.name
+            linkattributeName = self.linkattribute
+            self(self.scenario, linkattributeName, self.polygonfile, self.polygonfield, self.algorithm, self.excludeconnector, self.default)
+            self.tool_run_msg = _m.PageBuilder.format_info("Tool complete")
+        except Exception, e:
+            self.tool_run_msg = _m.PageBuilder.format_exception(e, _traceback.format_exc(e))
+
+    @_m.logbook_trace("Tag Polygon Attribute To Link")
+    def __call__(self,scenario,linkattributeName,polygonfile,polygonfield,algorithm,excludeconnector,default):
+        self.CheckAttribute(scenario, linkattributeName, default)
+        
+        import logging
+        logger = logging.getLogger()
+        logger.setLevel(logging.CRITICAL)
+        
+        emme_network = scenario.get_network()
+        
+        with fiona.open(polygonfile) as polygonshapefile:
+            polygon_meta = polygonshapefile.meta
+            
+            for linki in emme_network.links():
+            
+                if excludeconnector:
+                    if (int(linki.i_node.id)>99999) and (int(linki.j_node.id)>99999):
+                        pass # regular link, continue tagging
+                    else:
+                        #print("%s: IsConnector"%linki.id)
+                        continue #to next EMME link
+                
+                #convert emme link to line string object, note this is for TransLink's RTM specific projection
+                linkishape = [(x*1000, y*1000) for (x,y) in linki.shape]
+                line = geom.LineString(linkishape)
+                
+                #Check if the link has been tagged
+                With_match = False
+                
+                #check if emme link is completely within a polygon (most cases)
+                for polygonfeature in polygonshapefile:
+                    if line.within(geom.shape(polygonfeature["geometry"])):
+                        linki[linkattributeName] = polygonfeature["properties"][polygonfield]
+                        With_match = True
+                        #print(linki.id)
+                        break #stop looking for right polygon match
+                
+                #Multiple matching algorithm: tag_max; tag_min; by_length; do_not_change; set_to_-1.0
+                if not(With_match) and algorithm!="do_not_change":
+                    PercentIntersect = {}
+                    for polygonfeature in polygonshapefile:
+                        intersection = (geom.shape(polygonfeature["geometry"])).intersection(line)
+                        if not(intersection.is_empty):
+                            PercentIntersect[polygonfeature["properties"][polygonfield]]=intersection.length
+                    
+                    if PercentIntersect=={}:
+                        linki[linkattributeName] = default
+                        #print("%s: no match"%linki.id)
+                    elif algorithm=="tag_max":
+                        linki[linkattributeName]=max(list(PercentIntersect.keys()))
+                        #print("%s: tag_max"%linki.id)
+                    elif algorithm=="tag_min":
+                        linki[linkattributeName]=min(list(PercentIntersect.keys()))
+                        #print("%s: tag_min"%linki.id)
+                    elif algorithm=="by_length":
+                        linki[linkattributeName]=max(PercentIntersect, key=PercentIntersect.get)
+                        #print("%s: by_length"%linki.id)
+                    elif algorithm=="set_to_-1.0":
+                        linki[linkattributeName]=float(algorithm.split("_")[-1])
+                        #print("%s: set to %.0f"%(linki.id,float(algorithm.split("_")[-1])))
+                    else:
+                        linki[linkattributeName]=default
+                        #print("%s: no match"%linki.id)
+        
+        scenario.publish_network(emme_network)
+        
+        
+    def CheckAttribute(self, scenario, linkattributeName, default):
+        AttributeExist = scenario.extra_attribute(linkattributeName)
+        if AttributeExist:
+            pass
+        else:
+            create_attr = _m.Modeller().tool("inro.emme.data.extra_attribute.create_extra_attribute")
+            create_attr("LINK", linkattributeName,"tag polygon attribute", default, True, scenario)
---
+++ RTM/Scripts/Phase3Analytics/Variables_of_Interest_Extraction.py
@@ -0,0 +1,104 @@
+##---------------------------------------------------------------------
+##--TransLink Regional Transportation Model
+##--
+##--Path:
+##--Purpose: Export data for Machine Learning Algorithm Exploration
+##---------------------------------------------------------------------
+import inro.modeller as _m
+import inro.emme.desktop as _d
+import traceback as _traceback
+
+import os
+import re
+import csv as csv
+
+import sqlite3
+import pandas as pd
+import numpy as np
+
+
+
+class ExploreML(_m.Tool()):
+
+    ExcelSheetName = _m.Attribute(str)
+    tool_run_msg = _m.Attribute(unicode)
+    
+    def __init__(self):
+        self.ExcelSheetName = "Sheet1"
+    
+    def page(self):
+        pb = _m.ToolPageBuilder(self)
+        pb.title = "Export Data from Model Run"
+        pb.description = "Please make sure mf9999 and mo9999 are not in use."
+        pb.branding_text = "TransLink"
+        
+        pb.add_text_box(tool_attribute_name="ExcelSheetName", size=30, title="Worksheet Name for Variables of Interest ")
+
+        if self.tool_run_msg:
+            pb.add_html(self.tool_run_msg)
+
+        return pb.render()
+
+    def run(self):
+        self.tool_run_msg = ""
+        try:
+            eb = _m.Modeller().emmebank
+            self.__call__(eb, self.ExcelSheetName)
+            self.tool_run_msg = _m.PageBuilder.format_info("Tool complete")
+        except Exception, e:
+            self.tool_run_msg = _m.PageBuilder.format_exception(e, _traceback.format_exc(e))
+            
+        util = _m.Modeller().tool("translink.util")
+        util.delmat(eb, "mf9999")
+        util.delmat(eb, "mo9999")
+
+
+    @_m.logbook_trace("Data Export")
+    def __call__(self, eb, ExcelSheetName):
+        util = _m.Modeller().tool("translink.util")
+        
+        #load variable_of_interest table
+        #VOI_df = pd.read_csv("ExploreML/VOI_Table.csv")
+        VOI_df = pd.read_excel("ExploreML/Variables_of_Interest.xlsx",sheetname=ExcelSheetName)
+        
+        self.get_ML_DataExport(eb, VOI_df)
+
+        
+    def get_ML_DataExport(self, eb, VOI_df):
+        util = _m.Modeller().tool("translink.util")
+        
+        #initialize temporary matrix for data export
+        util.initmat(eb, "mf9999", "tempExploreMLMatrix", "temp matrix for ExploreML", 0) #for matrix data
+        util.initmat(eb, "mo9999", "tempExploreMLVector", "temp vector for ExploreML", 0) #for vector data
+        
+        conn = sqlite3.connect("ExploreML/ExploreML.db")
+        
+        #iterate through table and add mf data
+        SQL_TableList = list(set(VOI_df["Category"].tolist())) #get unique sql table names
+        for SQL_Table in SQL_TableList:
+            
+            #look up SQL table type: matrix or vector
+            TableType = VOI_df.loc[VOI_df.Category==SQL_Table,"Dimension"].tolist()[0]
+            
+            #initialize an empty mf long dataframe
+            if TableType == "matrix":
+                df = util.get_pd_ij_df(eb).astype(int)
+                tempmatrix = "mf9999"
+            elif TableType == "vector":
+                df = pd.DataFrame()
+                df["TAZ"] = util.get_matrix_numpy(eb, "mozoneindex", reshape=False).astype(int)
+                tempmatrix = "mo9999"
+            else:
+                raise Exception("Table dimension is not recognized for Category: %s"%SQL_Table)
+            
+            Attribute_df = VOI_df[VOI_df.Category==SQL_Table]
+            for index,row in Attribute_df.iterrows():
+            
+                #compute matrix expression into temp matrix
+                specs = []
+                specs.append(util.matrix_spec(tempmatrix, row["MatrixExpression"]))
+                util.compute_matrix(specs)
+                df[row["Attributes"]] = util.get_matrix_numpy(eb, tempmatrix).flatten()
+                
+            df.to_sql(name=SQL_Table, con=conn, flavor='sqlite', index=False, if_exists='replace')
+        conn.close()
