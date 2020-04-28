2cf26f7f9967a32d00bdb116890f23d06b9adca0
Reid Keller <reid.keller@translink.ca>
2020-04-28 14:51:12 -0700

    Remove reference to the RTMTools toolbox in the relocate_tools files This was a place holder but didnt end up getting used.

--- RTM/Scripts/relocate_tools.sh
+++ RTM/Scripts/relocate_tools.sh
@@ -7,4 +7,3 @@ PYTHON=$(cygpath "$EMMEPATH")/Python27/python.exe
 "$PYTHON" toolbox_modify.py ../Scripts/Phase3Scripts/RTM_Phase3.mtbx Phase3Scripts
 "$PYTHON" toolbox_modify.py ../Scripts/util/RTM_util.mtbx util
 "$PYTHON" toolbox_modify.py ../Scripts/Phase3Analytics/RTM3Analytics.mtbx Phase3Analytics
-"$PYTHON" toolbox_modify.py ../Scripts/Phase3Tools/RTM3Tools.mtbx Phase3Tools
